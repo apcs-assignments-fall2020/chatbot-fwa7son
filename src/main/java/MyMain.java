1d9c2d061ad85034f637417ec040edbdb98ad949
fwa7son <21fwatson@athenian.org>
2020-09-01 22:40:05 +0000

    added 5 questions and responses

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,10 +1,27 @@
 import java.util.Scanner;
 
 public class MyMain {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(final String[] args) {
+        final Scanner scan = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        final String name = scan.next();
+        System.out.println("Nice to meet you, " + name);
+        
+        System.out.print("What is your favorite breed of dog? ");
+        final String dog = scan.next();
+        System.out.println("Thats a great breed! I also like " + dog);
 
+        System.out.print("How old are you? ");
+        final String age = scan.next();
+        System.out.println("Wow thats older than me by " + age);
 
+        System.out.print("How are you today? (one word answer)");
+        final String today = scan.next();
+        System.out.println("I have had many days like that... A week ago I was " + today);
+
+        System.out.print("What grade are you in? ");
+        final String grade = scan.next();
+        System.out.println("I haven't gone to school but my favorite grade is also " + grade);
         scan.close();
     }
 }
